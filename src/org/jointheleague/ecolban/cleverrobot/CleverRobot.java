4c24640c0e2d8088b47cbb91fedbea7f71759d15
sdvic <gvw@san.rr.com>
2017-02-27 21:21:43 -0800

    All 3 ultrasonic sensors working...rev

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -6,37 +6,52 @@ import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
 import org.jointheleague.ecolban.rpirobot.IRobotInterface;
 import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 
-public class CleverRobot extends IRobotAdapter {
-	
-	public CleverRobot(IRobotInterface iRobot) {
-		super(iRobot);
-	}
+public class CleverRobot extends IRobotAdapter
+{
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("Try event listner, rev Saturday2130");
-		IRobotInterface base = new SimpleIRobot();
-		CleverRobot rob = new CleverRobot(base);
-		rob.getGoing();
-	}
+    public CleverRobot(IRobotInterface iRobot)
+    {
+	super(iRobot);
+    }
 
-	private void getGoing() {
-		Sonar sonar = new Sonar();
-		for (int i = 0; i < 100000; i++) {
-			System.out.println("+" + sonar.readSonar());
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-			}
-		}
-		try {
-			shutDown();
-		} catch (IOException e) {
-		}
-	}
+    public static void main(String[] args) throws Exception
+    {
+	System.out.println("Try event listner, rev Monday 2030");
+	IRobotInterface base = new SimpleIRobot();
+	CleverRobot rob = new CleverRobot(base);
+	rob.getGoing();
+    }
 
-	private void shutDown() throws IOException {
-		reset();
-		stop();
-		closeConnection();
+    private void getGoing()
+    {
+	Sonar sonar = new Sonar();
+	for (int i = 0; i < 100000; i++)
+	{
+	    try
+	    {
+		System.out.print(sonar.readSonar("left") + "     ");
+		Thread.sleep(300);
+		System.out.print(sonar.readSonar("center") + "     ");
+		Thread.sleep(300);
+		System.out.print(sonar.readSonar("right") + "\n");
+		Thread.sleep(300);
+	    } catch (Exception e)
+	    {
+		System.out.println("readSonar exception");
+	    }
 	}
+	try
+	{
+	    shutDown();
+	} catch (IOException e)
+	{
+	}
+    }
+
+    private void shutDown() throws IOException
+    {
+	reset();
+	stop();
+	closeConnection();
+    }
 }
--- src/org/jointheleague/ecolban/cleverrobot/Sonar.java
+++ src/org/jointheleague/ecolban/cleverrobot/Sonar.java
@@ -11,41 +11,124 @@ import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Sonar
 {
-	private long startTime;
-	private long stopTime;
-	final GpioController gpio = GpioFactory.getInstance();
-	private GpioPinDigitalOutput strobe = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);// Strobe...pin 11
-	private GpioPinInput echo = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03); // Echo...pin 15
-	private int filteredDistance;
+    private long startTime;
+    private long stopTime;
+    final GpioController gpio = GpioFactory.getInstance();
+    private GpioPinDigitalOutput rightStrobe = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);// Strobe...pin
+												// 13...right
+    private GpioPinInput rightEcho = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05); // Echo...pin
+										      // 18...right
+    private GpioPinDigitalOutput leftStrobe = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);// Strobe...pin
+											       // 12...left
+    private GpioPinInput leftEcho = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04); // Echo...pin
+										     // 16...left
+    private GpioPinDigitalOutput centerStrobe = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);// Strobe...pin
+												 // 11...center
+    private GpioPinInput centerEcho = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03); // Echo...pin
+										       // 15...center
+    private int rightFilteredDistance;
+    private int leftFilteredDistance;
+    private int centerFilteredDistance;
 
-	public Sonar()
+    public Sonar()
+    {
+	rightEcho.addListener(new GpioPinListenerDigital()
 	{
-		echo.addListener(new GpioPinListenerDigital()
+	    @Override
+	    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
+	    {
+		if (event.getState() == PinState.HIGH)
 		{
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
-			{
-				if (event.getState() == PinState.HIGH)
-				{
-					startTime = System.nanoTime();
-				}
-				if (event.getState() == PinState.LOW)
-				{
-					stopTime = System.nanoTime();
-				}
-				int distance = (int) ((((stopTime - startTime) / 1e3) / 2) / 29.1); // cm
-				if (distance > 0 && distance < 1000)
-				{
-					filteredDistance = distance;
-				}
-			}
-		});
-	}
-
-	public int readSonar()
+		    startTime = System.nanoTime();
+		}
+		if (event.getState() == PinState.LOW)
+		{
+		    stopTime = System.nanoTime();
+		}
+		int distance = (int) ((((stopTime - startTime) / 1e3) / 2) / 29.1); // cm
+		if (distance > 0 && distance < 1000)
+		{
+		    rightFilteredDistance = distance;
+		}
+		if (distance > 1000)
+		{
+		    return;
+		}
+	    }
+	});
+	leftEcho.addListener(new GpioPinListenerDigital()
 	{
-		strobe.high();
-		strobe.low();
-		return filteredDistance;
+	    @Override
+	    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
+	    {
+		if (event.getState() == PinState.HIGH)
+		{
+		    startTime = System.nanoTime();
+		}
+		if (event.getState() == PinState.LOW)
+		{
+		    stopTime = System.nanoTime();
+		}
+		int distance = (int) ((((stopTime - startTime) / 1e3) / 2) / 29.1); // cm
+		if (distance > 0 && distance < 1000)
+		{
+		    leftFilteredDistance = distance;
+		}
+		if (distance > 1000)
+		{
+		    return;
+		}
+	    }
+	});
+	centerEcho.addListener(new GpioPinListenerDigital()
+	{
+	    @Override
+	    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
+	    {
+		if (event.getState() == PinState.HIGH)
+		{
+		    startTime = System.nanoTime();
+		}
+		if (event.getState() == PinState.LOW)
+		{
+		    stopTime = System.nanoTime();
+		}
+		int distance = (int) ((((stopTime - startTime) / 1e3) / 2) / 29.1); // cm
+		if (distance > 0 && distance < 1000)
+		{
+		   centerFilteredDistance = distance;
+		}
+		if (distance > 1000)
+		{
+		    return;
+		}
+	    }
+	});
+    }
+
+    public int readSonar(String position) throws InterruptedException
+    {
+	if (position.equals("right"))
+	{
+	    rightStrobe.low();
+	    rightStrobe.high();
+	    rightStrobe.low();
+	    return rightFilteredDistance;
 	}
+	if (position.equals("left"))
+	{
+	    leftStrobe.low();
+	    leftStrobe.high();
+	    leftStrobe.low();
+	    return leftFilteredDistance;
+	}
+	if (position.equals("center"))
+	{
+	    centerStrobe.low();
+	    centerStrobe.high();
+	    centerStrobe.low();
+	    return centerFilteredDistance;
+	}
+	return -1;
+    }
 }
